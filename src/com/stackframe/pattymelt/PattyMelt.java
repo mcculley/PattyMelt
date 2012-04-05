b99c9124b95a1b6599380da21cdd276a72766fee
Gene McCulley <mcculley@stackframe.com>
2012-04-05 17:54:21 -0400

    Add binary loading.

--- src/com/stackframe/pattymelt/PattyMelt.java
+++ src/com/stackframe/pattymelt/PattyMelt.java
@@ -28,8 +28,11 @@
 package com.stackframe.pattymelt;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * A simple command line driver for DCPU-16.
@@ -39,6 +42,33 @@ import java.io.IOException;
 public class PattyMelt {
 
     /**
+     * Load a binary file into memory.
+     *
+     * @param memory a ShortBuffer to read the file into
+     * @param file a File to read from
+     * @throws IOException
+     */
+    private static void loadBinary(short[] memory, File file) throws IOException {
+        // FIXME: Close streams correctly.
+        int i = 0;
+        InputStream inputStream = new FileInputStream(file);
+        while (true) {
+            int v1 = inputStream.read();
+            if (v1 == -1) {
+                return;
+            }
+
+            int v2 = inputStream.read();
+            if (v2 == -1) {
+                return;
+            }
+
+            short value = (short) ((v2 << 8) | v1);
+            memory[i++] = value;
+        }
+    }
+
+    /**
      * Load a file into memory. The file is assumed to be lines of hexadecimal
      * 16-bit words.
      *
@@ -46,7 +76,8 @@ public class PattyMelt {
      * @param reader a BufferedReader to read from
      * @throws IOException
      */
-    private static void load(short[] memory, BufferedReader reader) throws IOException {
+    private static void loadHex(short[] memory, BufferedReader reader) throws IOException {
+        // FIXME: Close streams correctly.
         int i = 0;
         while (true) {
             String line = reader.readLine();
@@ -57,6 +88,40 @@ public class PattyMelt {
             short value = (short) Integer.parseInt(line, 16);
             memory[i++] = value;
         }
+    }
+
+    private static boolean isBinary(File file) throws IOException {
+        // FIXME: Close streams correctly.
+        InputStream inputStream = new FileInputStream(file);
+        while (true) {
+            int value = inputStream.read();
+            if (value == -1) {
+                return false;
+            }
+
+            char c = (char) value;
+            boolean isLetterOrDigit = Character.isLetterOrDigit(c);
+            boolean isWhitespace = Character.isWhitespace(c);
+            if (!(isLetterOrDigit || isWhitespace)) {
+                return true;
+            }
+        }
+    }
+
+    private static String dumpMemory(int address, int numWords, short[] memory) {
+        // FIXME: Make this better. It is a crude bit of debugging right now.
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < numWords; i++) {
+            short value = memory[address + i];
+            char c = (char) value;
+            if (!(Character.isLetterOrDigit(c) || Character.isWhitespace(c))) {
+                c = '.';
+            }
+
+            buf.append(c);
+        }
+
+        return buf.toString();
     }
 
     private static String dumpState(DCPU16 cpu) {
@@ -70,15 +135,29 @@ public class PattyMelt {
     public static void main(String[] args) throws Exception {
         DCPU16 cpu = new DCPU16Emulator();
         String filename = args[0];
+        short[] memory = cpu.memory();
         System.err.println("Loading " + filename);
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
-        load(cpu.memory(), reader);
+        File file = new File(filename);
+
+        // Try to guess if this is binary or not. Should add an option to be explicit.
+        if (isBinary(file)) {
+            loadBinary(memory, file);
+        } else {
+            // FIXME: Close streams correctly.
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            loadHex(memory, reader);
+        }
+
         final String header = "PC   SP   OV   SKIP A    B    C    X    Y    Z    I    J\n"
                 + "---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----";
         System.out.println(header);
-        while (true) {
-            System.out.println(dumpState(cpu));
-            cpu.step();
+        try {
+            while (true) {
+                System.out.println(dumpState(cpu));
+                cpu.step();
+            }
+        } catch (IllegalOpcodeException ioe) {
+            System.err.printf("Illegal opcode 0x%04x encountered.\n", ioe.opcode);
         }
     }
 }
