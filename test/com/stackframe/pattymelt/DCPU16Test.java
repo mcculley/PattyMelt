33d0383a2b9c4dcf1b8bb05ece3f7b06d1fd0f30
Gene McCulley <mcculley@stackframe.com>
2012-04-06 11:07:15 -0400

    Flesh out test of initial state.

--- test/com/stackframe/pattymelt/DCPU16Test.java
+++ test/com/stackframe/pattymelt/DCPU16Test.java
@@ -42,7 +42,15 @@ public class DCPU16Test {
         DCPU16 cpu = new DCPU16Emulator();
         short[] memory = cpu.memory();
         for (int i = 0; i < 0x10000; i++) {
-            assertEquals(0, memory[i]);
+            assertEquals("memory at " + i, 0, memory[i]);
         }
+
+        for (DCPU16.Register r : DCPU16.Register.values()) {
+            assertEquals(r.name(), 0, cpu.register(r));
+        }
+
+        assertEquals("PC", 0, cpu.PC());
+        assertEquals("SP", (short) 0xFFFF, (short) cpu.SP()); // FIXME: Not sure if this is what SP should be at initial state. Should it be zero?
+        assertEquals("O", 0, cpu.O());
     }
 }
